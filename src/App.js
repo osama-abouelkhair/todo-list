4ad4026aa2a92a6240613c59ea5d78113b78a6cc
osama abo elkhair <osama.abulkhair@gmail.com>
2020-08-17 23:57:20 +0200

    Implement first componenets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav/Nav'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+ 
+class Nav extends Component {
+    
+    constructor(props){
+        super(props);
+    }
+
+    render () {
+        return (
+            <div>
+                <p>nav</p>
+            </div>
+        )
+    }
+}
+
+export default Nav
